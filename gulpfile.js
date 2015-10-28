df3e3c0493dc9cb698c523a1c861be7c28863aa8
ajwarnick <junkwarnick@gmail.com>
2015-10-28 18:44:48 -0400

    gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var concat = require('gulp-concat');
 
 gulp.task('default', function(){
   // Default task code
+});
+
+
+
+
+ 
+gulp.task('scripts', function() {
+  return gulp.src(['/components/bootstrap/css/bootstrap.css'],
+            {base: 'bower_components/'})
+    //.pipe(concat('all.css'))
+    .pipe(gulp.dest('./css/'));
 });
